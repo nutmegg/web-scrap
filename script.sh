056c2a8c6775e6e3c250c1027aa390d441bbb807
subhash2807 <lcs2019016@iiitl.ac.in>
2020-07-22 00:33:05 +0000

    converted to csv

--- script.sh
+++ script.sh
@@ -1,3 +1,10 @@
 #! /bin/bash
 
-pdftotext -layout NIRF-ENGG-INF-312.pdf -|sed '$d'| awk 'NF{print $0}'| sed -r 's/  +/,/g'| sed -r 's/^,//g' | ./script.awk > d.csv
+n=0;
+DIR=$1
+for file in $DIR/*
+do
+	pdftotext -layout $file -|sed '$d'| awk 'NF{print $0}'| sed -r 's/  +/,/g'| sed -r 's/^,//g' | ./script.awk > "$(echo $file | sed 's/..\/nirf\///'| sed 's/\.pdf/\.csv/' )"
+
+	#echo "$(echo $file | sed 's/..\/nirf\///'| sed 's/\.pdf/\.csv/' )"
+done
