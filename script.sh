09f901bb0f48cea5c153773daff7f81265252c30
subhash2807 <lcs2019016@iiitl.ac.in>
2020-07-31 12:49:14 +0000

    final commit

--- script.sh
+++ script.sh
@@ -4,7 +4,7 @@ n=0;
 DIR=$1
 for file in $DIR/*
 do
-	pdftotext -layout $file -|sed '$d'| awk 'NF{print $0}'| sed -r 's/  +/,/g'| sed -r 's/^,//g' | ./script.awk > "$(echo $file | sed 's/..\/nirf\///'| sed 's/\.pdf/\.csv/' )"
+	pdftotext -layout $file -|sed '$d'| awk 'NF{print $0}'| sed -r 's/  +/,/g'| sed -r 's/^,//g' | ./script.awk > ./output/$(echo $file | sed 's/..\/nirf\///'| sed 's/\.pdf/\.csv/' )
 
 	#echo "$(echo $file | sed 's/..\/nirf\///'| sed 's/\.pdf/\.csv/' )"
 done
